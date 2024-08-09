3330a28256e647197e218dd685f2d87378bbee33
Oscar Gomez <oscargomezgomezluna@gmail.com>
2024-08-09 18:36:20 -0500

    POS-05 create component and show multiple posts

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from "react";
+import Post from "../components/Post";
 
 function Posts(){
-  const [posts, setPosts] = useState();
+  const [posts, setPosts] = useState(null);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(posts => setPosts(posts))
   }, [])
-  console.log(posts)
+
   return (
-    <>
-    </>
+    <div className="flex items-center justify-center min-h-screen p-4">
+      <ul className="flex flex-wrap gap-4 justify-center">
+        {posts?.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </ul>
+    </div>
   )
 }
 export default Posts;
+
+
