53251bcd17d2d9df8988184e6e679f50a036c016
Santiago <santiago.garcia.2007.22@gmail.com>
2024-08-13 13:26:26 -0500

    POS-07 create a custom hook useUserId and implement it in Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useUserId } from '../hooks/useUserId';
 
 const Post = ({post}) => {
+  const user = useUserId(post.userId);
   return (
     <li className="flex-1 min-w-[300px] border border-gray-300 rounded-lg p-4 bg-white shadow-md" >
-      <h2>Body: {post.body}</h2>
-      <p>Title: {post.title}</p>
+      <h2> {post.title} </h2>
+      <p>Posted by: { user && post.Id}</p>
+      <p> {post.body} </p>
     </li>    
+
   )
 }
 
