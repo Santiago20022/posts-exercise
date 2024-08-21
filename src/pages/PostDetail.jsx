a82be0065acb689d028219569dd4d64d2d739b04
Oscar Gomez <oscargomezgomezluna@gmail.com>
2024-08-21 10:26:04 -0500

    POS-10_Create_validation_post_deatail

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -6,21 +6,29 @@ const PostDetail = () => {
   const {id} = useParams();
   const [post, setPost] = useState(null);
   const [comments, setComments] = useState([]);
+  const [postNotFound, setPostNotFound] = useState(true);
  
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then(response => response.json())
-      .then(postData => setPost(postData));
+      .then(response => {response.json()} )
+      .then(postData => {
+        if (postData && Object.keys(postData).length > 0) {
+          setPost(postData);
+          setPostNotFound(false);
+        } else {
+          setPost(null);
+          setPostNotFound(true);
+        }
+      });
     
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
       .then(response => response.json())
       .then(commentsData => setComments(commentsData));
-
   }, [id]);
 
   return (
     <div className="container mx-auto p-4">
-      {post && (
+      {postNotFound ? (
         <div className="mb-4 p-4">
           <div className="flex items-center mb-2">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 mr-2">
@@ -29,23 +37,26 @@ const PostDetail = () => {
             </svg>
             <strong className='text-xl'>Post title</strong>
           </div>
-          <h1 className="mb-4 text-2xl font-bold">{post.title}</h1>
+          <h1 className="mb-4 text-2xl font-bold">{post?.title}</h1>
           <div className="flex items-center mb-2">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 mr-2">
               <path fillRule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75H12a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
             </svg>
             <strong>Description</strong>
           </div>
-          <p>{post.body}</p>
-        </div>
-      )}
-      <div>
-        <ul>
-          {comments?.map((comment) => (
-            <Comment key={comment.id} comment={comment} />
-          ))}
-        </ul>
+            <p>{post?.body}</p>
+          <div>
+            <ul>
+            {comments?.map((comment) => (
+              <Comment key={comment.id} comment={comment} />
+            ))}
+            </ul>
       </div>
+        </div>
+      ) : (
+        <p className="text-xl font-bold">Post not found</p>
+      )}
+      
     </div>
   )
 }
