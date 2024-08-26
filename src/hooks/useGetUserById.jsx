708e92835875642f208221107209c27e3f988dd5
Santiago <santiago.garcia.2007.22@gmail.com>
2024-08-26 16:57:23 -0500

    fix indentation

--- src/hooks/useGetUserById.jsx
+++ src/hooks/useGetUserById.jsx
@@ -4,6 +4,4 @@ import { UsersContext } from '../context/UsersContext';
 export const useGetUserById = (userId) => {
   const { users } = useContext(UsersContext);
   return users.find((user) => user.id === parseInt(userId));
-
-  
 };
